3c9b6c3876120085a867a552adc4e4fe0957eae0
digbyk <digby@digby.net>
2015-04-08 21:53:13 +0100

    qweqwe

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send('Hello Worlds!');
 });
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var server = app.listen(port, function () {
-
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-
+app.listen(server_port, server_ip_address, function () {
+	console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
