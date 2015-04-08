809ad1e54e6bab14d29f7606d2b8b649f0dcac9e
digbyk <digby@digby.net>
2015-04-08 22:06:47 +0100

    qweqwe

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'ssshhhhh',
+	resave: false,
+	saveUninitialized: false
+}));
 
 app.get('/', function (req, res) {
 	res.send('Hello Worlds!');
