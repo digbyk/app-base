5bdadacc51a4ba897aecf1d3a2e54e5c7b0cbf95
digbyk <digby@digby.net>
2015-04-08 22:18:57 +0100

    qqwe

--- app.js
+++ app.js
@@ -28,10 +28,13 @@ app.use(session({
 }));
 
 app.get('/', function (req, res) {
-	res.send('Hello Worlds!');
+	//res.send('Hello Worlds!');
+	res.render("index", {
+		user: 'digby'
+	});
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.listen(server_port, server_ip_address, function () {
