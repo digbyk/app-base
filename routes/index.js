8649df50e647927d5e50a55af8b4ce54ce126f04
digbyk <digby@digby.net>
2015-04-09 22:28:42 +0100

    Adding service (start of mongo integration)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var dbService = require('../services/db-service.js');
+
 module.exports = function () {
 	router.get('/', function (req, res) {
 		res.render("index", {
-			user: 'Digby'
+			user: dbService.getData().name
 		});
 	});
 	return router;
