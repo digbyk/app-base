ec969145b3432cf0fcb7037eaf84cbc393dcad28
digbyk <digby@digby.net>
2015-04-12 00:10:05 +0100

    Fixing config again

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ var config = require('cloud-env');
 var mongoose = require('mongoose');
 var mongoUrl = config.get('MONGODB_DB_URL');
 
-//mongoose.connect('mongodb://mongo:mongo@ds061601.mongolab.com:61601/ostest');
-mongoose.connect(mongoUrl);
+mongoose.connect('mongodb://mongo:mongo@ds061601.mongolab.com:61601/ostest');
+//mongoose.connect(mongoUrl);
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
