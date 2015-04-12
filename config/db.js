c544d56e3d3dd5e134fb74ed788715d7142ee4ee
digbyk <digby@digby.net>
2015-04-12 01:00:13 +0100

    Changing Openshoft MongoURL to my env var

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var mongodbUrl = process.env.MONGODB_URL || process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017';
+var mongodbUrl = process.env.MONGODB_URL || process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017';
 
 mongoose.connect(mongodbUrl);
 var db = mongoose.connection;
